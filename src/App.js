f7c987e3eb0d97c1b3e092836b992838665c3fc9
unknown <limteikyee@gmail.com>
2019-03-14 13:21:08 +0800

    housekeeping

--- src/App.js
+++ src/App.js
@@ -5,43 +5,42 @@ import { toDoContainer } from './state'
 import './style/App.css'
 
 class App extends Component {
-    render() {
-        return (
-            <div className="app">
-                <NavBar />
-                <h1 className="title">To Do List</h1>
-                <Login />
-                <SignUp />
-                <Route
-                    // the button will be rendered with different props depend on what route
-                    exact
-                    path="/"
-                    render={() => {
-                        //update the filter based on the route
-                        toDoContainer.updateFilter('All')
-
-                        return <ButtonsFilter />
-                    }}
-                />
-                <Route
-                    path="/Active"
-                    render={() => {
-                        toDoContainer.updateFilter('Active')
-                        return <ButtonsFilter />
-                    }}
-                />
-                <Route
-                    path="/Done"
-                    render={() => {
-                        toDoContainer.updateFilter('Done')
-                        return <ButtonsFilter />
-                    }}
-                />
-                <Input />
-                <List />
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="app">
+				<NavBar />
+				<h1 className="title">To Do List</h1>
+				<Login />
+				<SignUp />
+				<Route
+					// the button will be rendered with different props depend on what route
+					exact
+					path="/"
+					render={() => {
+						//update the filter based on the route
+						toDoContainer.updateFilter('All')
+						return <ButtonsFilter />
+					}}
+				/>
+				<Route
+					path="/Active"
+					render={() => {
+						toDoContainer.updateFilter('Active')
+						return <ButtonsFilter />
+					}}
+				/>
+				<Route
+					path="/Done"
+					render={() => {
+						toDoContainer.updateFilter('Done')
+						return <ButtonsFilter />
+					}}
+				/>
+				<Input />
+				<List />
+			</div>
+		)
+	}
 }
 
 export default App
