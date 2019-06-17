3e1ef0b89d0ada188116beeaf7d9b8cd79ad5c82
tylim <5227509+tylim88@users.noreply.github.com>
2019-06-17 11:47:43 +0800

    Update index.js

--- src/state/index.js
+++ src/state/index.js
@@ -4,4 +4,4 @@ import { UserContainer } from './userState'
 const toDoContainer = new ToDoContainer()
 const userContainer = new UserContainer()
 
-export { UserContainer, ToDoContainer, toDoContainer, userContainer }
+export { toDoContainer, userContainer }
